a50d6528d04bc92e9f8c0d26fbd4678061bcec1f
apoorvjauhari <154303290+apoorvjauhari@users.noreply.github.com>
2023-12-28 12:39:49 +0530

    Update

---
+++ app/src/main/java/com/example/cloudnotes/NewNote.kt
@@ -0,0 +1,11 @@
+package com.example.cloudnotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewNote : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_note)
+    }
+}
---
+++ app/src/main/java/com/example/cloudnotes/ViewNote.kt
@@ -0,0 +1,11 @@
+package com.example.cloudnotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewNote : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_note)
+    }
+}
